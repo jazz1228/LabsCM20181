4c9f9e019c66078056ff84fe4369b98a841fd34b
Juan <juan.zapata19@udea.edu.co>
2018-02-23 12:00:48 -0500

    Dearr

--- Lab1/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20181/lab1/MainActivity.java
+++ Lab1/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20181/lab1/MainActivity.java
@@ -1,7 +1,9 @@
 package co.edu.udea.compumovil.gr04_20181.lab1;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Para Creae los Actions bar a mi gusto
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
+
+        //Flecha para regresar a la anterior activity
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 
--- Lab1/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20181/lab1/splash_screen.java
+++ Lab1/Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20181/lab1/splash_screen.java
@@ -1,6 +1,7 @@
 package co.edu.udea.compumovil.gr04_20181.lab1;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,7 @@ public class splash_screen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
         // Esconde el actionbar para esta actividad
+
          getSupportActionBar().hide();
 
 
